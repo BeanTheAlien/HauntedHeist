42364a7047fb91e1130b5eadf1a57ce0bc97c420
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-12-11 14:24:15 -0600

    Introduce food and water management in Person class
    Added food and water attributes with methods to manage them.

--- person.py
+++ person.py
@@ -1,4 +1,15 @@
 class Person:
   def __init__(self, name):
     self.__name = name
-    self.__hunger = 0
+    self.__food = 100
+    self.__water = 100
+  def feed(self, amount):
+    self.__food += amount
+  def starve(self, amount):
+    self.__food -= amount
+  def name(self):
+    return self.__name
+  def food(self):
+    return self.__food
+  def water(self):
+    return self.__water
