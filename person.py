bb9d619ffc38ce353657f467dd527c524001e9cf
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-12-11 19:39:06 -0600

    Minor updates

--- person.py
+++ person.py
@@ -3,13 +3,35 @@ class Person:
     self.__name = name
     self.__food = 100
     self.__water = 100
-  def feed(self, amount):
+  def feed(self, amount: int):
     self.__food += amount
-  def starve(self, amount):
+  def starve(self, amount: int):
     self.__food -= amount
-  def name(self):
+  
+  def name(self) -> str:
+    """
+    Returns the name of the person.
+    
+    :param self: This person.
+    :return: The name.
+    :rtype: str
+    """
     return self.__name
-  def food(self):
+  def food(self) -> int:
+    """
+    Returns the current percent food level of the person.
+    
+    :param self: This person.
+    :return: The food level.
+    :rtype: int
+    """
     return self.__food
-  def water(self):
+  def water(self) -> int:
+    """
+    Returns the current percent water level of the person.
+    
+    :param self: This person.
+    :return: The water level.
+    :rtype: int
+    """
     return self.__water
