fcc9be0c25757d383cf06c90cc900c97b44697c2
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-12-13 13:40:44 -0600

    Major overhaul and updates

--- person.py
+++ person.py
@@ -1,69 +1,50 @@
-from schedule import Skd
-
 class Person:
   def __init__(self, name: str):
-    self.__name: str = name
-    self.__food: float = 100
-    self.__water: float = 100
-    self.__skd: list[Skd] = []
-    self.__state: int = 0
-  
-  def feed(self, amount: float) -> float:
-    self.__food += amount
-    return self.__food
-  def starve(self, amount: float) -> float:
-    self.__food -= amount
-    return self.__food
-  def skdAdd(self, entry: Skd) -> list[Skd]:
-    self.__skd.append(entry)
-    return self.__skd
-  def skdRem(self, entry: Skd) -> list[Skd]:
-    self.__skd.remove(entry)
-    return self.__skd
-  
-  def name(self) -> str:
-    """
-    Returns the name of the person.
-    
-    :param self: This person.
-    :return: The name.
-    :rtype: str
-    """
-    return self.__name
-  def food(self) -> float:
-    """
-    Returns the current percent food level of the person.
-    
-    :param self: This person.
-    :return: The food level.
-    :rtype: float
-    """
-    return self.__food
-  def water(self) -> float:
-    """
-    Returns the current percent water level of the person.
-    
-    :param self: This person.
-    :return: The water level.
-    :rtype: float
-    """
-    return self.__water
-  def skd(self) -> list[Skd]:
-    return self.__skd
-  def die(self):
-    # need implementation within unreal (nitro python runtime could work)
-    self.__state = 1
-    print("dead")
-  
-  def upd(self):
-    if self.__state == 1: return
-    elif self.__food <= 0 or self.__water <= 0:
-      self.die()
+    self.name: str = name
+    self.food: int = 100
+    self.water: int = 100
+    self.bored: int = 0
+    self.danger: int = 0
+    self.tired: int = 0
+    self.money: int = 1000
 
+    self.Food = self.attrib("food")
+    self.Water = self.attrib("water")
+    self.Bored = self.attrib("bored")
+    self.Danger = self.attrib("danger")
+    self.Tired = self.attrib("tired")
+    self.Money = self.attrib("money")
+
+    self.eat = self.act("eat", lambda: self.food <= 70, "food", 10)
+  
+  def attrib(self, attName: str):
+    return lambda amt = None : self.att(attName, amt if amt else 0)
+  def att(self, attName: str, amt: int):
+    self.apnd(attName, amt)
+    return self.get(attName)
+  
+  def act(self, nm: str, cond, prop: str, amt: int):
+    self.set(nm, cond)
+    self.set(nm.title(), lambda: self.apnd(prop, amt))
+  
+  def add(self, attName: str, amt: int):
+    self.apnd(attName, amt)
+    self.clamp(attName, 100)
+  
+  def get(self, attName: str):
+    return getattr(self, attName)
+  def set(self, attName: str, val):
+    setattr(self, attName, val)
+  def apnd(self, attName: str, val):
+    self.set(attName, self.get(attName) + val)
+  def clamp(self, attName: str, lim: int):
+    v = self.get(attName)
+    self.set(attName, lim if v > lim else v)
+  
   def __str__(self):
-    return f"-- Person --\nName: \"{self.__name}\"\nFood: {self.__food}\nWater: {self.__water}"
+    return f"-- Person --\nName: \"{self.name}\"\nFood: {self.food}\nWater: {self.water}"
 
 person = Person("Hello World")
 print(person)
-person.starve(100)
-person.upd()
+person.Food(2)
+print(person.Food())
--- schedule.py
+++
@@ -1,5 +0,0 @@
-class Skd:
-    def __init__(self, name: str):
-        self.__name: str = name
-    def name(self) -> str:
-        return self.__name
