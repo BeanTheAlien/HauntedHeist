9242a441ba74a7885f554d58b099e9b5826797d0
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-01-20 08:24:53 -0600

    I forgor

--- person.py
+++ person.py
@@ -8,12 +8,27 @@ class Person:
         self.tired: int = 0
         self.alive: bool = True
     def upd(self):
-        if not self.alive:
+        if self.isDead():
             return
         if self.hunger < 0 or self.thirst < 0:
             self.die()
             return
+        if not self.inDanger():
+            if self.isHungry() or self.isThirsty():
+                pass
+            elif self.isTired():
+                pass
+            elif self.isBored():
+                pass
+        else:
+            pass
     def die(self):
         self.alive = False
+    def isHungry(self) -> bool: return self.hunger < 70
+    def isThirsty(self) -> bool: return self.thirst < 70
+    def isBored(self) -> bool: return self.bored > 40
+    def inDanger(self) -> bool: return self.danger > 30
+    def isTired(self) -> bool: return self.tired > 60
+    def isDead(self) -> bool: return not self.alive
 
 person: Person = Person("John Person")
